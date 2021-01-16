0d51c14470c5c6648762ab9eca137400f6398432
jjsilas <jjsilas@gmail.com>
2021-01-16 07:10:25 -0600

    I updated js, and now the search is working, but I have to leave for a couple of day and may not get a chance to work on this further before the submission is due.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,33 @@
 $(document).ready(() => {
-    $('#searchBar').on('submit', (e) => {
+    $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
-        getFilms(searchText);
+        getMovies(searchText);
         e.preventDefault();
     });
 });
 
-function getFilms(searchText) {
-    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=195cf56c')
+function getMovies(searchText) {
+    axios.get('http://www.omdbapi.com/?s=' + searchText + '&tt3896198&apikey=195cf56c')
         .then((response) => {
             console.log(response);
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie) => {
+                output += `
+            <div class="col-md-3">
+              <div class="well text-center">
+              <h4>${movie.Title}</h4>
+                <img src="${movie.Poster}">
+                
+                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary"</a>
+              </div>
+            </div>
+          `;
+            });
+
+            $('#movies').html(output);
         })
         .catch((err) => {
             console.log(err);
-
         });
 }
